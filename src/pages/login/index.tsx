aefbc9838b5382ca69830c889c3554de9084acc7
chaitawat-dev <chaitawat.ho@gmail.com>
2022-12-25 00:58:18 +0700

    restructure files

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,42 @@
+// ** React Imports
+import { useState, ReactNode, MouseEvent } from 'react'
+
+// ** Next Imports
+import Link from 'next/link'
+
+// ** MUI Components
+import Alert from '@mui/material/Alert'
+import Button from '@mui/material/Button'
+import Divider from '@mui/material/Divider'
+import Checkbox from '@mui/material/Checkbox'
+import TextField from '@mui/material/TextField'
+import InputLabel from '@mui/material/InputLabel'
+import IconButton from '@mui/material/IconButton'
+import Box, { BoxProps } from '@mui/material/Box'
+import FormControl from '@mui/material/FormControl'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import OutlinedInput from '@mui/material/OutlinedInput'
+import { styled, useTheme } from '@mui/material/styles'
+import FormHelperText from '@mui/material/FormHelperText'
+import InputAdornment from '@mui/material/InputAdornment'
+import Typography, { TypographyProps } from '@mui/material/Typography'
+import MuiFormControlLabel, { FormControlLabelProps } from '@mui/material/FormControlLabel'
+
+const LoginWrapper = styled(Box)<BoxProps>(({ theme }) => {
+  return {
+    height: '100vh',
+    padding: theme.spacing(20),
+    '& .content-left': {
+      display: 'flex',
+      minHeight: '100vh',
+      overflowX: 'hidden',
+      position: 'relative'
+    }
+  }
+})
+
+function index() {
+  return <LoginWrapper>Login</LoginWrapper>
+}
+
+export default index
