ab36158ff333810c09522983d60ab71b3aa66e76
chaitawat-dev <chaitawat.ho@gmail.com>
2022-12-27 16:40:55 +0700

    filter card

--- src/pages/apps/products/index.tsx
+++ src/pages/apps/products/index.tsx
@@ -1,5 +1,16 @@
+import { Box, Card as MuiCard, styled, Typography } from '@mui/material'
 import React from 'react'
 
+const Card = styled(MuiCard)(({ theme }) => ({
+  padding: 20
+}))
+
 export default function index() {
-  return <div>index</div>
+  return (
+    <Box>
+      <Card>
+        <Typography variant='h6'>Filters</Typography>
+      </Card>
+    </Box>
+  )
 }
